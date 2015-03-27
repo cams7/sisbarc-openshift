3591ae258296ecce3887120fc16b17dcdc176411
César Magalhães <ceanma@gmail.com>
2015-03-27 09:33:54 -0300

    Algumas correções foram feitas

--- maven-projects/java/paas/acende_apaga_leds-ejb/src/main/java/org/jboss/as/quickstarts/ear/ejb/GreeterEJB.java
+++ maven-projects/java/paas/acende_apaga_leds-ejb/src/main/java/org/jboss/as/quickstarts/ear/ejb/GreeterEJB.java
@@ -50,10 +50,10 @@ public class GreeterEJB {
 				.getRequestContext();
 		if ("vbox".equalsIgnoreCase(name))
 			context.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-					"http://192.168.1.5:8181/acende_apaga_leds/sayhello");
+					"http://200.141.106.145:8081/acende_apaga_leds/sayhello");
 		else if ("vaio".equalsIgnoreCase(name))
 			context.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-					"http://192.168.1.7:8181/acende_apaga_leds/sayhello");
+					"http://200.141.106.145:8080/acende_apaga_leds/sayhello");
 
 		Map<String, List<String>> headers = new HashMap<String, List<String>>();
 		headers.put("username", Collections.singletonList("ceanma@gmail.com"));
